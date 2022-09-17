d35c2a8a71c040354db58e30f2408ed78e9708c0
Brennan <bmtymrak@gmail.com>
2022-09-16 22:38:55 -0400

    Created Customer and Hazard models

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Customer(models.Model):
+
+    BLOWING_DIRECTION_CHOICES = [
+        ("N", "North"),
+        ("S", "South"),
+        ("E", "East"),
+        ("W", "West"),
+    ]
+
+    first_name = models.CharField(null=True, max_length=100)
+    last_name = models.CharField(null=True, max_length=100)
+    driver = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    address = models.CharField(null=True, blank=True, max_length=250)
+    zip_code = models.IntegerField(null=True, blank=True)
+    blowing_direction = models.CharField(
+        max_length=3, choices=BLOWING_DIRECTION_CHOICES
+    )
+    notes = models.TextField(null=True, blank=True)
+
+
+class Hazard(models.Model):
+    content = models.CharField(null=True, blank=True, max_length=250)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=False)
