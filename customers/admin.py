6f242282e8e166c3152a164e79e18f7199639bd8
Brennan <bmtymrak@gmail.com>
2022-09-28 21:38:51 -0400

    Register Hazard to admin

--- customers/admin.py
+++ customers/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from customers.models import Customer
+from customers.models import Customer, Hazard
 
 
 admin.site.register(Customer)
+
+admin.site.register(Hazard)
