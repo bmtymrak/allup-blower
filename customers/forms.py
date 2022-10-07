8ad5382bb1de5045ffced694372b8c2dfcc1d192
Brennan <bmtymrak@gmail.com>
2022-10-06 21:53:00 -0400

    Customer add form styling

--- customers/forms.py
+++ customers/forms.py
@@ -1,17 +1,20 @@
 from django.forms import ModelForm, inlineformset_factory
-
 from customers.models import Customer, Hazard
 
 
 class CustomerForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["first_name"].widget.attrs["placeholder"] = "First Name"
-        self.fields["last_name"].widget.attrs["placeholder"] = "Last Name"
+        self.fields["first_name"].widget.attrs.update(
+            placeholder="First Name", size="12"
+        )
+        self.fields["last_name"].widget.attrs.update(placeholder="Last Name", size="12")
         self.fields["address"].widget.attrs["placeholder"] = "Address"
-        self.fields["zip_code"].widget.attrs["placeholder"] = "Zipcode"
+        self.fields["zip_code"].widget.attrs.update(placeholder="Zipcode")
         self.fields["blowing_direction"].widget.attrs["placeholder"] = "Direction"
-        self.fields["notes"].widget.attrs["placeholder"] = "Notes"
+        self.fields["notes"].widget.attrs.update(
+            placeholder="Notes", cols="20", rows="5"
+        )
 
     class Meta:
         model = Customer
