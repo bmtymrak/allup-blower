60f775282e2b65fea700b855296e6ab68d9a77a9
Brennan <bmtymrak@gmail.com>
2022-10-24 21:49:24 -0400

    Change model CharFields to blank==True

--- customers/models.py
+++ customers/models.py
@@ -18,17 +18,17 @@ class Customer(models.Model):
         ("W", "West"),
     ]
 
-    first_name = models.CharField(null=True, max_length=100)
-    last_name = models.CharField(null=True, max_length=100)
+    first_name = models.CharField(blank=True, max_length=100)
+    last_name = models.CharField(blank=True, max_length=100)
     driver = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True
     )
-    address = models.CharField(null=True, blank=True, max_length=250)
+    address = models.CharField(blank=False, max_length=250)
     zip_code = models.IntegerField(null=True, blank=True)
     blowing_direction = models.CharField(
         blank=True, max_length=3, choices=BLOWING_DIRECTION_CHOICES
     )
-    notes = models.TextField(null=True, blank=True)
+    notes = models.TextField(blank=True)
     photo = models.ImageField(storage=PrivateMediaStorage(), null=True, blank=True)
 
     def save(self, *args, **kwargs):
@@ -52,7 +52,7 @@ class Customer(models.Model):
 
 
 class Hazard(models.Model):
-    content = models.CharField(null=True, blank=True, max_length=250)
+    content = models.CharField(blank=True, max_length=250)
     customer = models.ForeignKey(
         Customer, on_delete=models.CASCADE, null=False, related_name="hazards"
     )
